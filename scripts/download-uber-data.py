e87a623a5434cb91ab1ba0bea2075313fc0c3d8d
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-07-03 09:33:28 +0530

    Add code to download travel time between pairs of wards

--- scripts/download-uber-data.py
+++ scripts/download-uber-data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from datetime import datetime, timedelta
 import json
+import os
 from os.path import abspath, dirname, join, exists
 import sys
 
@@ -30,6 +31,7 @@ def dump_ward_pairs(include_reverse=True):
             return json.load(f)
 
     data = get_bmtc_routes()
+    print("Dumping pairs of wards for {} bus routes ".format(len(data)))
     wards = get_wards()
     ward_properties = {
         int(ward["properties"]["WARD_NO"]): ward["properties"]
@@ -63,5 +65,77 @@ def dump_ward_pairs(include_reverse=True):
     return movement_pairs
 
 
+def get_travel_time(
+    src, dst, time_range=None, time_period="ALL_DAY", days_of_week=None
+):
+    """Fetch travel time between a source and a destination.
+
+    src and dst are Movement IDs for wards. These can be obtained from the ward
+    boundaries dataset provided by Uber.
+
+    The function uses API calls to Uber's backend, and you need to set a couple
+    of environment variables for this function to work - MOVEMENT_WEB_COOKIE
+    and MOVEMENT_CSRF_TOKEN. These can be obtained by visiting
+    movement.uber.com in your browser and Network tab of the developer console
+    in your browser.
+
+    """
+
+    if days_of_week is None:
+        days_of_week = [1, 2, 3, 4, 5, 6, 7]
+
+    if time_range:
+        start, end = time_range
+    else:
+        delta = timedelta(seconds=19800)
+        start = int((datetime(2018, 10, 1) + delta).strftime("%s"))
+        end = int((datetime(2018, 12, 31) + delta).strftime("%s"))
+
+    cookies = {"web-movement:sess": os.environ["MOVEMENT_WEB_COOKIE"]}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0",
+        "x-csrf-token": os.environ["MOVEMENT_CSRF_TOKEN"],
+        "Content-Type": "application/json",
+    }
+    params = (("rpc", "GET_DETAILED_TRAVEL_TIMES"),)
+    data = {
+        "query": {
+            "cityId": 130,
+            "sourceId": src,
+            "dataSet": "wards",
+            "destinationId": dst,
+            "timeRanges": [{"startTime": start, "endTime": end}],
+            "daysOfWeek": days_of_week,
+            "timePeriodBucket": time_period,
+        }
+    }
+
+    response = requests.post(
+        "https://movement.uber.com/_rpc",
+        headers=headers,
+        params=params,
+        cookies=cookies,
+        data=json.dumps(data),
+    )
+    assert response.status_code == 200
+    assert response.json()["status"] == "success"
+    return response.json()["data"]
+
+
+def download_uber_travel_time_data():
+    pairs = dump_ward_pairs()
+    print("Fetching travel time for {} pairs".format(len(pairs)))
+
+    travel_time_data = dict()
+    for i, ((src_w_id, src_m_id), (dst_w_id, dst_m_id)) in enumerate(pairs):
+        travel_time = get_travel_time(src_m_id, dst_m_id)
+        print(i, (src_w_id, dst_w_id))
+        travel_time_data["{}-{}".format(src_w_id, dst_w_id)] = travel_time
+
+    path = join(DATA, "uber-travel-time-data.json")
+    with open(path, "w") as f:
+        json.dump(travel_time_data, f, indent=2)
+
+
 if __name__ == "__main__":
-    dump_ward_pairs()
+    download_uber_travel_time_data()
